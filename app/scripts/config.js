de0fc753c71ed4a0597e4afb7ceb8d638fb47bf7
Ralph Yozzo <fedex1@gmail.com>
2014-05-04 23:44:14 -0400

    add backend support

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -8,7 +8,8 @@ define([''], function () {
         //return 'http://udacity-conf.appspot.com';
         //return 'http://localhost:8080'
         //return 'http://localhost:9000'
-        return 'http://info-311.appspot.com'
+        //return 'http://info-311.appspot.com'
+        return 'http://311.brooklyncoop.org'
         //return 'http://install-metro.appspot.com'
     }
 
